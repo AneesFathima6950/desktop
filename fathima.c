0c05c97ca7cf181d7054dc8b1cf2d39dec8f4fec
AneesFathima6950 <104298432+AneesFathima6950@users.noreply.github.com>
2022-04-24 15:49:18 +0530

    created fathima.c
    conditional statement 2

---
+++ fathima.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int num=8;
+  switch(num)
+  {
+    case 1:
+      printf("value is 1");
+      break;
+   case 2:
+      printf("value is 2");
+      break;
+       case 3:
+      printf("value is 3");
+      break;
+   default:
+      printf("out of range");
+      break;
+  }
+  return 0;
+}
+
+      
